d0e839fcaced5d6b327a6e45e8bb7a798d267d98
Alien <40964633+Eve124i318074658073416@users.noreply.github.com>
2020-06-29 19:49:59 +0500

    changes to the comment

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        //Setting scanner
+        //Setting up scanner
         Scanner scan = new Scanner(System.in);
 
         //ROBOT #1
